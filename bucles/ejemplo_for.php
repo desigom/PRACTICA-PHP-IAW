0b6ed9b4f12497b955c5a52c32bc5f9b5497d8d2
desigom <desigomen@gmail.com>
2021-02-25 12:16:59 +0100

    ejemplos bucles

---
+++ bucles/ejemplo_for.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1>Ascendente</h1>";
+
+for ($i =1; $i <= 10; $i++) {
+    echo $i;
+    echo "<br>";
+}
+
+echo "<h1>Descendente</h1>";
+
+for ($i =10; $i >= 0; $i--) {
+    echo $i;
+    echo "<br>";
+}
+?>
---
+++ bucles/ejemplo_while.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<h1>While Ascendente</h1>";
+
+$i = 1;
+while ($i <= 10) {
+    echo $i;
+    echo "<br>";
+    $i++;
+}
+
+echo "<h1>While Descendente</h1>";
+
+$i = 10;
+while ($i >= 0) {
+    echo $i;
+    echo "<br>";
+    $i--;
+}
+
+
+?>
