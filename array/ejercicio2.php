8af1c840b8dc0c816dc417ce3d8bdfd76347a514
desigom <desigomen@gmail.com>
2021-03-05 19:59:45 +0100

    ejercicios array

---
+++ array/ejercicio2.php
@@ -0,0 +1,23 @@
+<?php
+// Definimos una constante con el numero de elementos del array
+define("num_elementos", 100);
+
+// Paso 1. Declaración del array
+$lista = array();
+
+// Paso 2. Inicialización del array
+for ($i = 0; $i < num_elementos; $i++ ){
+    $lista[$i] = rand(1, 30);
+}
+
+// Paso 3. Imprimir el array
+for ($i = 0; $i < num_elementos; $i++ ) {
+    echo $lista[$i];
+    echo "<br>";
+}
+
+$media = array_sum($lista)/count($lista);
+
+echo "La media es: ".$media;
+
+?>
